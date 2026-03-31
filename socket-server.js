75e48ff3e455f63003518298f20b6a2e578ee822
Ashish2343 <a98126959@gmail.com>
2026-03-31 13:33:49 +0530

    frontend-url

--- socket-server.js
+++ socket-server.js
@@ -72,6 +72,6 @@ io.on("connection", (socket) => {
 
 
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT,"0.0.0.0", () => {
   console.log(`server is running on port http://localhost:${PORT}`)
 });
